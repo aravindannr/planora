33dfe7989565b9fd583716efdd40219ed1b8ed20
Aravindan N R <aravindannr59@gmail.com>
2026-01-20 18:46:59 +0530

    removed unwanted lines

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:planora/core/theme/app_theme.dart';
 import 'package:planora/core/theme/theme_notifier.dart';
